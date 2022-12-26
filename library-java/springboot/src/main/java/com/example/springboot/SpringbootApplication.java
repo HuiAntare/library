d49a9bd962bb57760c190a5562aac533f01f7579
HuiAntare <1468627709@qq.com>
2022-12-26 22:07:08 +0800

    图书管理系统后端

--- library-java/springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ library-java/springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,7 +2,6 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +13,9 @@ public class SpringbootApplication {
     }
 
 
-    //健康检查接口,返回success
-    @GetMapping()
-    public String health(){
-        return "success";
-    }
+//    //健康检查接口,返回success
+//    @GetMapping()
+//    public String health(){
+//        return "success";
+//    }
 }
